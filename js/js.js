d19deb209a632bd3bc1f23cc76f9b47071552fc7
Andrew K <a.koval1996@gmail.com>
2017-09-29 19:27:55 +0200

    skill section + image onclick (1st one)

--- js/js.js
+++ js/js.js
@@ -23,20 +23,21 @@ function initMap() {
 
 /*/ Get the modal*/
 var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-//var img = document.getElementsByClassName('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+
+for ( i=0; i<=9; i++) {
+    var img = document.getElementsByClassName("img-modal")[i];
+
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
